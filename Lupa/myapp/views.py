a8ba9231be9b421182b38b3fbe415cee7d77e17c
francisco <fran.gonzaleza@duocuc.cl>
2024-06-26 18:44:12 -0400

    ola

---
+++ Lupa/myapp/views.py
@@ -0,0 +1,12 @@
+# Create your views here.
+from django.shortcuts import render
+
+
+def login_view(request):
+    return render(request, 'paginas/Login.html')
+
+def prueba_view(request):
+    return render(request, 'paginas/prueba.html')
+
+def Nosotros_view(request):
+    return render(request, 'paginas/Nosotros.html')
